59074d16b2c068c6f82ba47168f32be0b63259e0
Kevin Karma <tenngawang6080@gmail.com>
2024-10-26 19:44:27 -0400

    homepage setup

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,39 @@ export default function Home() {
         <section className="flex-grow w-full">
           <h1 className="primary-heading">Personal Color Palette Generator</h1>
           <p>Easy to use, powered by AI.</p>
+          <div className="flex flex-col gap-8 mt-16">
+            <div className="flex justify-start items-center gap-7">
+              <Image 
+                src="/assets/icons/step_1.svg"
+                width={70}
+                height={70}
+                alt="Step 1 icon"
+              />
+              <p className="inline text-3xl font-extrabold capitalize">upload image</p>
+            </div>
+            <div className="flex justify-start items-center gap-7">
+              <Image 
+                src="/assets/icons/step_1.svg"
+                width={70}
+                height={70}
+                alt="Step 1 icon"
+              />
+              <p className="inline text-3xl font-extrabold capitalize">upload image</p>
+            </div>
+            <div className="flex justify-start items-center gap-7">
+              <Image 
+                src="/assets/icons/step_1.svg"
+                width={70}
+                height={70}
+                alt="Step 1 icon"
+              />
+              <p className="inline text-3xl font-extrabold capitalize">upload image</p>
+            </div>
+          </div>
         </section>
         <section className="flex-grow w-full">
           <Image 
-            className="w-[580px] object-cover"
+            className="w-full object-cover"
             src={"/assets/images/homepage_primary.png"}
             width={1000}
             height={1000}
